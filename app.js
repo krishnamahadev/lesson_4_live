8fa74f2af895b1b5254e8f396d8dc02d82e85df3
Krishna M <krishna.mahadevaiah@gmail.com>
2022-09-27 07:21:59 +0530

    alert and prompt

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 
 // checking if its working
 console.log("Hey it is working!")
+
+
+// two ways of using Javascript 1. Prompt 2. Alert
+// alert("Hey welome!")
+
+// var x = prompt("What is your name buddy")
+
+// alert("Hey welcome "+x+" Javascript world")
+
+
+
+
+// button is created in HTML. I am try to reference that button from JS and also play around with it. This is called DOM scripting.
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+console.log(btnTranslate);
