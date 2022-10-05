b3d3845e1ec9cc56fd972451668ab9b8c219001b
Krishna M <krishna.mahadevaiah@gmail.com>
2022-10-05 09:38:15 +0530

    update minion

--- app1.js
+++ app1.js
@@ -6,7 +6,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputresult = document.querySelector("#output")
 
-var serverURL = "https://api.funtranslations.com/translate/vulcan.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function gettranslateURL(text)
 {
